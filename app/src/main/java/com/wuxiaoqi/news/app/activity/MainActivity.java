1486b014114a1c917f8c8db548adfabb4b92325e
wuxiaoqi <1321972760@qq.com>
2017-09-19 09:14:35 +0800

    按两次退出

--- app/src/main/java/com/wuxiaoqi/news/app/activity/MainActivity.java
+++ app/src/main/java/com/wuxiaoqi/news/app/activity/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.wuxiaoqi.news.app.R;
+import com.wuxiaoqi.news.app.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    private long onBackTime = 0;
 
     public static void startActivity(Activity activity) {
         Intent i = new Intent(activity, MainActivity.class);
@@ -15,8 +19,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void initData(Bundle bundle) {
+
+    }
+
+    @Override
+    public int getContentView() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initEvent() {
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - onBackTime > 2000) {
+            Toast.makeText(this, R.string.press_the_exit_again, Toast.LENGTH_SHORT).show();
+            onBackTime = System.currentTimeMillis();
+        } else super.onBackPressed();
     }
 }
